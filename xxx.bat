e773db9339160c4c7ac9d12ea96d272fce135579
tale123456789 <tale123456789@gmail.com>
2019-06-26 22:36:16 +0800

    Delete xxx.bat

--- xxx.bat
+++
@@ -1,6 +0,0 @@
-@echo off
- for %%i in (./list/*.txt) do echo %%i
- rem for /l %%i in (1,1,5) do @echo %%i
- rem for %%i in (./list/*.txt) do echo %%~ni
- for %%i in (./list/*.txt) do copy sample.tif .\tif\%%~ni.tif
-pause
